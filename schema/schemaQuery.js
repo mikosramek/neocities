5ad62529f9f4c9200a5e4bedcdde5c86795b43cf
mikosramek <miko@mikosramek.ca>
2022-04-07 21:07:54 -0400

    fix: schema files added

---
+++ schema/schemaQuery.js
@@ -0,0 +1,42 @@
+// import fetch from 'node-fetch';
+const fs = require('fs');
+const path = require('path')
+
+const schema = require('./schema.json')
+
+// Go to graphql url
+// run this query
+// past this into schema.json
+// then run `npm run get-schema`
+const url = 'https://allmanorofduns.prismic.io/graphql';
+const query = `
+  {
+    __schema {
+      types {
+        kind
+        name
+        possibleTypes {
+          name 
+        }
+      }
+    }
+  }
+`
+
+const writeData = () => {
+    // here we're filtering out any type information unrelated to unions or interfaces
+    const filteredData = schema.data.__schema.types.filter(
+      type => type.possibleTypes !== null,
+    );
+
+    schema.data.__schema.types = filteredData;
+    fs.writeFileSync(path.resolve(__dirname, 'fragmentTypes.json'), JSON.stringify(schema.data), err => {
+      if (err) {
+        console.error('Error writing fragmentTypes file', err);
+      } else {
+        console.log('Fragment types successfully extracted!');
+      }
+    });
+  };
+
+  writeData();
