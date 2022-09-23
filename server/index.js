4f43e328c3a74d4dcaa2b8714fc07ae567a56ccd
Miko Sramek <mikoggsramek@hotmail.com>
2022-09-23 13:08:14 -0400

    express cleanup w/ secret check

--- server/index.js
+++ server/index.js
@@ -1,25 +1,39 @@
 const express = require('express');
 const cors = require('cors');
-const publish = require('../site-publisher')
+const bodyParser = require('body-parser');
 const { exec } = require("child_process");
+
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json({ limit: '1mb' }))
+app.use(bodyParser.urlencoded({
+    extended: true,
+    limit: '1mb',
+}));
 
 
 app.post('/publish', async (req, res) => {
     console.log('publish webhook hit')
     try {
-        // await publish()
-        exec('make publish', (err, stdout, stderr) => {
-            if (err) {
-                console.err(err);
-                res.send(err);
-            } else {
-                console.log(stdout, stderr);
-                res.sendStatus(201);
-            }
-        })
+        const { secret } = req.body;
+        if (secret === process.env.PRISMIC_WEBHOOK_SECRET) {
+            exec('make publish', (err, stdout, stderr) => {
+                if (err) {
+                    console.err(err);
+                    res.send(err);
+                } else if (stderr) {
+                    console.log(stderr);
+                    res.sendStatus(500);
+                } else {
+                    console.log(stdout);
+                    res.sendStatus(201);
+                }
+            })
+        } else {
+            res.sendStatus(401);
+        }
     } catch (e) {
         res.send(e)
     }
